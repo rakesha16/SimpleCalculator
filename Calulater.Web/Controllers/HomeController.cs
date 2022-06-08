02d18249bdf5cddbfa5c235594582c93d85f3b50
rakesha16 <76029038+rakesha16@users.noreply.github.com>
2022-06-08 18:09:07 +0530

    Add files via upload

---
+++ Calulater.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Calulater.Core.Bal;
+using Calulater.Core.Contracts;
+using Calulater.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Accounts()
+        {      
+            return View();
+        }
+
+        public JsonResult Operation(UserEnterdDetails details)
+        {
+            GenericOperationFactory factary = new GenericOperationFactory();
+            IOperater operation = factary.GetOperationType(details.OperationSelected);
+            float result = operation.Calculate(details.ValueOne, details.ValueTwo);
+            var jsonReader = default(dynamic);
+            jsonReader = new
+            {
+                Value = result,               
+            };
+            return Json(jsonReader, JsonRequestBehavior.AllowGet); 
+        }
+    }   
+}
---
+++ Calulater.Web/Models/UserEnteredDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calulater.Web.Models
+{
+    public class UserEnterdDetails
+    {
+        public int OperationSelected { get; set; }
+        public float ValueOne { get; set; }
+        public float ValueTwo { get; set; }
+    }
+}
